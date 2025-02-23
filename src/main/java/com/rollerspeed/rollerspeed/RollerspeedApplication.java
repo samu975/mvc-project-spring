4bc465579919db35ea9bc8955c939c72990015ce
Samuel Rosero <saferoxx215@gmail.com>
2025-02-23 17:53:31 -0500

    configuracion inicial del proyecto rollerspeed

---
+++ src/main/java/com/rollerspeed/rollerspeed/RollerspeedApplication.java
@@ -0,0 +1,13 @@
+package com.rollerspeed.rollerspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollerspeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollerspeedApplication.class, args);
+	}
+
+}
