54fd737e012488b1a13974652bc2431eb60f4d9f
Samuel Rosero <saferoxx215@gmail.com>
2025-02-26 19:23:42 -0500

    add .env dependency, add the html files to template, create fragment for navbar and modify the HomeController

--- src/main/java/com/rollerspeed/rollerspeed/controller/HomeController.java
+++ src/main/java/com/rollerspeed/rollerspeed/controller/HomeController.java
@@ -9,12 +9,37 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "index"; // Renderiza la vista index.html
+        return "index";
     }
 
     @GetMapping("/acerca-de")
     public String acercaDe() {
-        return "acerca-de"; // Renderiza la vista acerca-de.html
+        return "acerca-de";
+    }
+
+    @GetMapping("/mision")
+    public String mision() {
+        return "mision";
+    }
+
+    @GetMapping("/vision")
+    public String vision() {
+        return "vision";
+    }
+
+    @GetMapping("/valores")
+    public String valores() {
+        return "valores";
+    }
+
+    @GetMapping("/servicios")
+    public String servicios() {
+        return "servicios";
+    }
+
+    @GetMapping("/eventos")
+    public String eventos() {
+        return "eventos";
     }
 
    
